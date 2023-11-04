eb96cd5910259a3ed3ede193b3469623c44636a4
epresmassive <125078452+epresmassive@users.noreply.github.com>
2023-11-04 21:20:41 +0100

    testing responsive navbar

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 let button = document.querySelector(".navbar-toggler");
 let headerVideo = document.querySelector("#header-video");
 let containerSlide = document.querySelector("#container-slide");
+let navbar = document.querySelector(".navbar");
 
 button.addEventListener("click", function() {
 let ariaExpanded = button.getAttribute("aria-expanded");
 
 if (ariaExpanded === "true") {
     // Set a different 'top' value when the navbar is expanded
-    headerVideo.style.top = "-230px";
-    containerSlide.style.top = "-230px";
+    headerVideo.style.top = "0px";
+    containerSlide.style.top = "0px";
+    navbar.style.backgroundColor = "rgba(131, 93, 46, 0.6)";
 } else {
     // Set the default 'top' value when the navbar is collapsed
     headerVideo.style.top = "-70px";
     containerSlide.style.top = "-70px";
+    navbar.style.backgroundColor = "rgba(35, 88, 123, 0.2)";
 }
 });
