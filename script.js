dbef55fd1b4ff96b4d99e5015efa99b4ab188224
epresmassive <125078452+epresmassive@users.noreply.github.com>
2023-11-12 17:07:50 +0100

    seperation of concern, mousevent handled in seperate file

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+// NAVBAR TOGGLER FOR SMALL SCREEN
 let toggler = document.querySelector(".navbar-toggler");
 let navbar = document.querySelector(".navbar");
 
-toggler.addEventListener("click", function() {
+toggler.addEventListener("click", function () {
     let ariaExpanded = toggler.getAttribute("aria-expanded");
 
     if (ariaExpanded === "true") {
@@ -9,4 +10,13 @@ toggler.addEventListener("click", function() {
     } else {
         navbar.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
     }
+});
+
+// MOUSE EVENT FOR THE CARDS
+var cards = document.querySelectorAll('.card');
+
+cards.forEach(function (card) {
+    card.addEventListener('mousedown', function () {
+        this.classList.toggle('active');
+    });
 });
